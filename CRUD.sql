86f04a5ec530a7f8f1c1299b0f4f55630891f1ca
TadjievCodes <muhammad2008.08@mail.ru>
2020-06-13 19:57:07 -0400

    UPDATE, DELETE STATEMENTS

--- CRUD.sql
+++ CRUD.sql
@@ -1,3 +1,41 @@
+--- DML - Data Manipulation Language - the subset of the SQL programming language that deals with CRUD operations.
+
+
+
+--An update statement for specific rows:
+
+UPDATE <table> SET <column> = <value> WHERE <condition>;
+--Examples:
+
+UPDATE users SET password = "thisisabadidea" WHERE id = 3;
+UPDATE blog_posts SET view_count = 1923 WHERE title = "SQL is Awesome";
+Update multiple columns for specific rows:
+
+UPDATE <table> SET <column 1> = <value 1>, <column 2> = <value 2> WHERE <condition>;
+--Examples:
+
+UPDATE users SET entry_url = "/home", last_login = "2016-01-05" WHERE id = 329;
+UPDATE products SET status = "SOLD OUT", availability = "In 1 Week" WHERE stock_count = 0;
+
+
+
+
+
+
+-- To delete specific rows from a table:
+
+DELETE FROM <table> WHERE <condition>;
+-- Examples:
+
+DELETE FROM users WHERE email = "andrew@teamtreehouse.com";
+DELETE FROM movies WHERE genre = "Musical";
+DELETE FROM products WHERE stock_count = 0;
+
+
+
+
+
+
 -- We've removed the reviews section from our website. Please remove all reviews.
 
 

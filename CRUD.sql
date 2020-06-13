b07765bb909292056a630da03d2a356dd1a09574
TadjievCodes <muhammad2008.08@mail.ru>
2020-06-13 19:40:04 -0400

    ADDED NEW CRUD

---
+++ CRUD.sql
@@ -0,0 +1,14 @@
+-- We've removed the reviews section from our website. Please remove all reviews.
+
+
+DELETE FROM reviews;
+
+
+-- Remove actors with the first name "Yuri", "Walter" and "Victor".
+
+---SELECT * FROM actors WHERE name LIKE "Yuri%" OR name LIKE "Walter%" OR name LIKE "Victor%";
+
+DELETE FROM actors WHERE name LIKE "Yuri%" OR name LIKE "Walter%" OR name LIKE "Victor%";
+
+
+

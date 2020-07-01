b606ec1d60a14104c64750f877669eb46b56f8ef
TadjievCodes <muhammad2008.08@mail.ru>
2020-07-01 00:54:58 -0400

    MAX (Id) Function

--- Week8.sql
+++ Week8.sql
@@ -1,3 +1,20 @@
+
+
+-- Week 8 Examples
+
+INSERT INTO Employees (Id, FirstName, LastName, City, Province, StreetAddress, PostalCode, AreaCode, PhoneNumber, BirthDate)
+VALUES (610, 'Ron', 'McLean', 'Oakville', 'ON', '231 Maple Ave', 'L4X-1L9', '289', '555-1551', '1960-04-12');
+
+
+
+
+
+
+
+
+
+-- LAB 6 Questions
+
 --- 1. Determine the last used employee Id
 
 SELECT MAX (Id) AS LastUsed

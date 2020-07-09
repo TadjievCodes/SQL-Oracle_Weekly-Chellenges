dab2b17bf5cedb39a4a79968919a4c7b3e46620d
TadjievCodes <muhammad2008.08@mail.ru>
2020-07-09 16:52:33 -0400

    substr and instr

--- Week9.sql
+++ Week9.sql
@@ -48,3 +48,58 @@ SELECT * FROM  Employees WHERE LOWER(lastname) = 'james';
 
 
 
+
+
+-- ctreating a table for practice
+
+CREATE table Contacts (name VARCHAR2(40));
+
+INSERT INTO contacts 
+VALUES ('LAM, MIKE, 416-384-3493');
+
+
+INSERT INTO contacts 
+VALUES ('Harvey, Annette, 905-868-2739');
+
+
+
+INSERT INTO contacts 
+VALUES ('Cobb, Ben, 416-239-3638');
+
+
+
+-- SUBSTR function - string -start -length
+
+SELECT DISTINCT postalcode,
+    SUBSTR(postalcode, 1, 3) AS "1st 3 bytes";
+    SUBSTR(postalcode, -3, 3) AS "last 3 bytes"
+FROM customers;
+
+
+-- INSTR FUNCTION
+-- (string, string to find, [start pos], [occurrence] 
+
+SELECT name,
+           INSTR(name, ',') AS "First comma pos.",
+           INSTR(name, ',', 10) AS "First comma after 9th byte pos.",
+           INSTR(name, ',', 1, 2) AS "2nd comma pos."
+FROM contacts;
+
+
+
+-- INSTR returns integer number and the SUBSTR returns a string
+-- mystery query with substr and instr
+
+
+SELECT name,
+           SUBSTR(name, 1, INSTR(name,',')-1) AS "Last",
+           SUBSTR(name,
+           INSTR(name, ',') +1,
+           INSTR(name, ',', 1, 2) -INSTR(name,',')-1) AS "First"
+FROM contacts;
+
+
+
+-- LPAD and RPAD example
+
+
